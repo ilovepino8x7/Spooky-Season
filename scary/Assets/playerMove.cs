ce49a051bf6a26579b11ef43076e818e9558d70e
ilovepino8x7 <teddybhunt@gmail.com>
2025-10-31 18:16:50 +0000

    Floating
    I created the movement and ability to phase through walls in the 'ghost' state (in the 'Start' scene)

--- scary/Assets/playerMove.cs
+++ scary/Assets/playerMove.cs
@@ -1,9 +1,12 @@
+using System.Collections;
 using UnityEngine;
 
 public class playerMove : MonoBehaviour
 {
     private int moveSpeed = 7;
     private bool grounded = false;
+    private bool isGhost = false;
+    private bool startedReset = false;
     private int moving = 0;
     private Rigidbody2D rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -13,6 +16,10 @@ public class playerMove : MonoBehaviour
     }
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.LeftShift) && !isGhost)
+        {
+            isGhost = true;
+        }
         if (Input.GetKey(KeyCode.A))
         {
             moving = -1;
@@ -25,28 +32,67 @@ public class playerMove : MonoBehaviour
         {
             moving = 0;
         }
-        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        if (Input.GetKeyDown(KeyCode.Space) && grounded && !isGhost)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, moveSpeed);
             grounded = false;
         }
         transform.rotation = Quaternion.identity;
+        if (isGhost)
+        {
+            rb.linearVelocity = new Vector2(0,0);
+            rb.gravityScale = 0;
+            gameObject.layer = 7;
+            if (Input.GetKey(KeyCode.A))
+            {
+                rb.linearVelocity = new Vector2(0,0);
+                transform.position += new Vector3(-moveSpeed * Time.deltaTime, 0);
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                rb.linearVelocity = new Vector2(0,0);
+                transform.position += new Vector3(moveSpeed * Time.deltaTime, 0);
+            }
+            else if (Input.GetKey(KeyCode.W))
+            {
+                rb.linearVelocity = new Vector2(0,0);
+                transform.position += new Vector3(0, moveSpeed * Time.deltaTime);
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                rb.linearVelocity = new Vector2(0,0);
+                transform.position += new Vector3(0, -moveSpeed * Time.deltaTime);
+            }
+            if (!startedReset)
+            {
+                startedReset = true;
+                StartCoroutine(resetToPlayer());
+            }
+        }
+        else
+        {
+            gameObject.layer = 6;
+        }
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (moving == -1)
+        if (!isGhost)
         {
-            rb.linearVelocity = new Vector2(-moveSpeed, rb.linearVelocity.y);
-        }
-        else if (moving == 1)
-        {
-            rb.linearVelocity = new Vector2(moveSpeed, rb.linearVelocity.y);
-        }
-        else if (moving == 0)
-        {
-            rb.linearVelocity = new Vector2 (0, rb.linearVelocity.y);
+            if (moving == -1)
+            {
+                rb.linearVelocity = new Vector2(-moveSpeed, rb.linearVelocity.y);
+            }
+            else if (moving == 1)
+            {
+                rb.linearVelocity = new Vector2(moveSpeed, rb.linearVelocity.y);
+            }
+            else if (moving == 0)
+            {
+                rb.linearVelocity = new Vector2(0, rb.linearVelocity.y);
+            }
+
         }
     }
     void OnCollisionEnter2D(Collision2D collision)
@@ -55,5 +101,12 @@ public class playerMove : MonoBehaviour
         {
             grounded = true;
         }
+    }
+    public IEnumerator resetToPlayer()
+    {
+        yield return new WaitForSeconds(3);
+        isGhost = false;
+        startedReset = false;
+        rb.gravityScale = 0.5f;
     }
 }
